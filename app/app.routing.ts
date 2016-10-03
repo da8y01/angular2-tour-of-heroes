6e47be41e8ee48f149d51d6ac38d1ef332009f19
Balrog <da8y_01@yahoo.com>
2016-10-03 18:26:16 -0500

    Routing.

---
+++ app/app.routing.ts
@@ -0,0 +1,27 @@
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HeroesComponent }      from './heroes.component';
+import { HeroDetailComponent }      from './hero-detail.component';
+import { DashboardComponent }      from './dashboard.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'detail/:id',
+    component: HeroDetailComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  }
+];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
